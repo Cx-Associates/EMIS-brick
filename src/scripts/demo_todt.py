233862ba9eaaa5ca6b8e739b6f9d31fe916b09cf
Daniel Jordan <daniel.jordan@cx-assoc.com>
2023-11-27 12:44:23 -0500

    prepping for demo

--- src/scripts/demo_todt.py
+++ src/scripts/demo_todt.py
@@ -2,15 +2,16 @@
 
 """
 from src.utils import Project
-from subrepos.energy_models.src.utils import TODTweekend
+from subrepos.energy_models.src.utils import TODT
 from copy import deepcopy
 
+# create an instance of the Project class
 project = Project(
     name='Main Street Landing',
     location=(44.48, -73.21),
 )
 project.set_time_frames(
-    baseline=('2023-11-08T00:00:00', '2023-11-25T00:00:00'),
+    baseline=('2023-11-08T00:00:00', '2023-11-27T00:00:00'),
 )
 time_frame = project.time_frames['baseline']
 graph_path = 'brick_models/msl_heating-only.ttl'
@@ -35,19 +36,33 @@ df_features['delta T'] = df['supply temp'] - df['return temp']
 
 ## new dataframe with btus proxy calc
 df_energy = df_features[['OAT']]
-df_energy['btus'] = df_features['pump speed'] * df_features['delta T']
+df_energy['Btus'] = df_features['pump speed'] * df_features['delta T']
 
 # make energy model from the dataframe
-energy_model = TODTweekend(
+energy_model = TODT(
     df_energy,
-    Y_col='btus',
+    Y_col='Btus',
     X_col='OAT'
 )
 energy_model.train(bins=None)
+energy_model.score()
 
+# plot of BTUs against OAT
+energy_model.scatterplot(x='OAT', y='Btus')
+energy_model.scatterplot()
+
+# plot of Y-predicted vs Y-actual
+energy_model.scatterplot()
+
+# now make a daily "model" by copying the model instance and resampling the data.
 energy_model_daily = deepcopy(energy_model)
-energy_model_daily.Y.test = energy_model_daily.Y.test.resample('d').sum()
-energy_model_daily.y.test = energy_model_daily.y.test.resample('d').sum()
+energy_model_daily.dataset = energy_model.dataset.resample('d').mean()
+energy_model_daily.Y.test = energy_model_daily.Y.test.resample('d').mean()
+energy_model_daily.y.test = energy_model_daily.y.test.resample('d').mean()
 energy_model_daily.score()
+energy_model_daily.scatterplot(x='OAT', y='Btus', alpha=.6)
+energy_model_daily.scatterplot()
+
+
 
 pass
