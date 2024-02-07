040d7b7180ce242f6bd9316c47ae9a24d9d53d5f
lizaboyle <liza.boyle@cx-assoc.com>
2024-02-07 11:39:17 -0500

    added correlations and some more Ace data

--- src/scripts/Dent_compile.py
+++ src/scripts/Dent_compile.py
@@ -50,8 +50,10 @@ for path in dentdatapath:
 
     # create 'time' column
     MSL_data1['CombinedDatetime']=pd.to_datetime(MSL_data1['Date '] + ' ' + MSL_data1['End Time '])
+    #^for some reason that has us off by 4 hours?
     MSL_data1.set_index('CombinedDatetime', inplace=True)
-    MSL_data1.index = MSL_data1.index.tz_localize('UTC').tz_convert(timezone)
+    MSL_data1.index = MSL_data1.index.tz_localize('US/Eastern', ambiguous='NaT')
+#    MSL_data1.index = MSL_data1.index.tz_localize('UTC').tz_convert(timezone)
     # Combine with existing data frame
     if 'MSL_data' in locals():
         MSL_data = pd.merge(MSL_data, MSL_data1, left_index=True, right_index=True, how='outer')
@@ -95,26 +97,29 @@ modelset.get_data()
 
 #end
 """
-str=[r'/cxa_main_st_landing/2404:9-240409/analogOutput/5/timeseries?start_time=2023-11-10&end_time=2023-12-31', #Pump 4a VFD Output
-r'/cxa_main_st_landing/2404:9-240409/analogOutput/6/timeseries?start_time=2023-11-10&end_time=2023-12-31', #Pump 4b VFD Output
-r'/cxa_main_st_landing/2404:9-240409/analogInput/15/timeseries?start_time=2023-11-10&end_time=2023-12-31', #Primary Hot Water Supply Temp_2
-r'/cxa_main_st_landing/2404:9-240409/analogInput/16/timeseries?start_time=2023-11-10&end_time=2023-12-31', #Primary Hot Water Return Temp_2
-r'/cxa_main_st_landing/2404:7-240407/analogValue/11/timeseries?start_time=2023-11-10&end_time=2023-12-31', #chilled water power meter
-r'/cxa_main_st_landing/2404:7-240407/analogOutput/4/timeseries?start_time=2023-11-10&end_time=2023-12-31', #pump 2a-b VFD output
-r'cxa/cxa_main_st_landing/2404:7-240407/binaryOutput/12/timeseries?start_time=2023-11-10&end_time=2023-12-31', #pump 2a active (binary)
-r'cxa/cxa_main_st_landing/2404:7-240407/binaryOutput/13/timeseries?start_time=2023-11-10&end_time=2023-12-31'] #pump 2b active (binary)
+str = [r'/cxa_main_st_landing/2404:9-240409/analogOutput/5/timeseries?start_time=2023-11-10&end_time=2024-01-06', #Pump 4a VFD Output
+r'/cxa_main_st_landing/2404:9-240409/analogOutput/6/timeseries?start_time=2023-11-10&end_time=2024-01-06', #Pump 4b VFD Output
+r'/cxa_main_st_landing/2404:9-240409/analogInput/15/timeseries?start_time=2023-11-10&end_time=2024-01-06', #Primary Hot Water Supply Temp_2
+r'/cxa_main_st_landing/2404:9-240409/analogInput/16/timeseries?start_time=2023-11-10&end_time=2024-01-06', #Primary Hot Water Return Temp_2
+r'/cxa_main_st_landing/2404:7-240407/analogValue/11/timeseries?start_time=2023-11-10&end_time=2024-01-06', #chilled water power meter
+r'/cxa_main_st_landing/2404:7-240407/analogOutput/4/timeseries?start_time=2023-11-10&end_time=2024-01-06', #pump 2a-b VFD output
+r'/cxa_main_st_landing/2404:7-240407/binaryOutput/12/timeseries?start_time=2023-11-10&end_time=2024-01-06', #pump 2a active (binary)
+r'/cxa_main_st_landing/2404:7-240407/binaryOutput/13/timeseries?start_time=2023-11-10&end_time=2024-01-06', #pump 2b active (binary)
+r'/cxa_main_st_landing/2404:2-240402/analogInput/10/timeseries?start_time=2023-11-10&end_time=2024-01-06', #P1a Feedback
+r'/cxa_main_st_landing/2404:10-240410/analogOutput/8/timeseries?start_time=2023-11-10&end_time=2024-01-06', #HRU Supplyfan VFD output
 
-headers=['Pump 4a VFD Output',
+headers = ['Pump 4a VFD Output',
          'Pump 4b VFD Output',
          'Primary Hot Water Supply Temp_2',
          'Primary Hot Water Return Temp_2',
          'chilled water power meter',
          'pump 2a-b VFD output',
          'pump 2a active',
-         'pump 2b active']
+         'pump 2b active',
+         'pump 1a feedback']
 
 #Lets try this
-#For each path for AceIoT data (listed above), get the data and put the data in the data frame
+#For each path for AceIoT data (listed above) and the description, get the data and put the data in the data frame with header listed above
 with open(env_filepath, 'r') as file:
     config = yaml.safe_load(file)
     url = config['DATABASE_URL']
@@ -136,6 +141,30 @@ with open(env_filepath, 'r') as file:
         else:
             msg = f'API request from ACE was unsuccessful. \n {res.reason} \n {res.content}'
             #raise Exception(msg)
+
+#Lets do some math to prepare for correlations!
+MSL_data['pump2a'] = MSL_data['pump 2a active']*MSL_data['pump 2a-b VFD output'] #this should be the BAS pump 2a power (ish)
+MSL_data['pump2b'] = MSL_data['pump 2b active']*MSL_data['pump 2a-b VFD output'] #same for pump 2b
+
+#Correlation time!
+#MSL_data.plot(x='pump2a', y='Avg. AmpL2 Phase_x')
+plt.plot(MSL_data['pump2a'], MSL_data['Avg. AmpL3 Phase_y'], marker='.', markersize=1)
+plt.xlabel('BAS VFD output')
+plt.ylabel('Dent Amp data for Pump 2a')
+plt.savefig(r'F:\PROJECTS\1715 Main Street Landing EMIS Pilot\code\Plots\Pump2aCorrelation.png')
+plt.close()
+
+plt.plot(MSL_data['pump2b'], MSL_data['Avg. AmpL2 Phase_y'], marker='.', markersize=1)
+plt.xlabel('BAS VFD output')
+plt.ylabel('Dent Power data for Pump 2b')
+plt.savefig(r'F:\PROJECTS\1715 Main Street Landing EMIS Pilot\code\Plots\Pump2bCorrelation.png')
+plt.close()
+
+plt.plot(MSL_data['pump 1a feedback'], MSL_data['Avg. AmpL1 Phase_y'], marker='.', markersize=1)
+plt.xlabel('BAS Pump 1a Feedback')
+plt.ylabel('Dent Power data for Pump 1')
+plt.savefig(r'F:\PROJECTS\1715 Main Street Landing EMIS Pilot\code\Plots\Pump1Correlation.png')
+plt.close()
 
 MSL_data.plot(y=['pump 2a-b VFD output',
          'pump 2a active',
